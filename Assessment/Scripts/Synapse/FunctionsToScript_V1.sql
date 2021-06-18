b2c07b8d20e04e061946fa43656d9b69e7574368
gailzmicrosoft <gazho@microsoft.com>
2021-06-18 18:41:45 -0400

    Added 7 Scripts and updated SQLScriptToRun.csv for Synapse. Fixed bug in AssessmentDriver_V2.ps1

---
+++ Assessment/Scripts/Synapse/FunctionsToScript_V1.sql
@@ -0,0 +1,9 @@
+Select '1' as Active, db_name() as DatabaseName, s.name as SchemaName, 
+'\' + db_name() + '\Functions\'  as OutputFolderPath,
+s.name + '_'+ o.name as FileName, 
+s.name + '.'+ o.name as 'ObjectName', 'FUNCTION' as ObjectsToScript 
+from sys.objects o
+inner join sys.schemas s 
+on o.schema_id = s.schema_id
+inner join sys.databases d
+on d.name = db_name()  and o.type_desc = 'SQL_SCALAR_FUNCTION'
